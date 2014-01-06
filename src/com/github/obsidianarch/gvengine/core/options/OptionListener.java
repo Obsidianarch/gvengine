787f4ae4f2e343f93df4891e5eab86700624453d
Obsidianarch <obsidianarch@yahoo.com>
2014-01-05 22:03:12 -0600

    Updated OptionManager

---
+++ src/com/github/obsidianarch/gvengine/core/options/OptionListener.java
@@ -0,0 +1,14 @@
+package com.github.obsidianarch.gvengine.core.options;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention( RetentionPolicy.RUNTIME )
+@Target( ElementType.METHOD )
+public @interface OptionListener {
+    
+    String[] fields();
+    
+}
