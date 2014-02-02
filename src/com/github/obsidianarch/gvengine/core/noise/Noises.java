02b0e40ee3e9f9fbd3c6a52f070225f89ebd8007
Obsidianarch <obsidianarch@yahoo.com>
2014-02-02 14:45:50 -0600

    Forgot to include the noise classes

---
+++ src/com/github/obsidianarch/gvengine/core/noise/Noises.java
@@ -0,0 +1,57 @@
+package com.github.obsidianarch.gvengine.core.noise;
+
+/**
+ * Provides access to many different methods of noise generation.
+ * 
+ * @author Austin
+ * @see <a href="http://webstaff.itn.liu.se/~stegu/simplexnoise/simplexnoise.pdf">Simplex
+ *      Noise</a>
+ */
+public final class Noises {
+    
+    /**
+     * Creates noise, based on the simplex noise algorithm, ranging from {@code [-1, 1]}.
+     * 
+     * @param x
+     *            The x input.
+     * @param y
+     *            The y input.
+     * @return Noise ranging from {@code [-1, 1]}.
+     */
+    public static double simplex2D( double x, double y ) {
+        return SimplexNoise.noise( x, y );
+    }
+    
+    /**
+     * Creates noise, based on the simplex noise algorithm, rangine from {@code [-1, 1]}.
+     * 
+     * @param x
+     *            The x input.
+     * @param y
+     *            The y input.
+     * @param z
+     *            The z input.
+     * @return Noise ranging from {@code [-1, 1]}.
+     */
+    public static double simplex3D( double x, double y, double z ) {
+        return SimplexNoise.noise( x, y, z );
+    }
+    
+    /**
+     * Creates noise, based on the simplex noise algorithm, rangine from {@code [-1, 1]}.
+     * 
+     * @param x
+     *            The x input.
+     * @param y
+     *            The y input.
+     * @param z
+     *            The z input.
+     * @param w
+     *            The w input.
+     * @return Noise ranging from {@code [-1, 1]}.
+     */
+    public static double simplex4D( double x, double y, double z, double w ) {
+        return SimplexNoise.noise( x, y, z, w );
+    }
+    
+}
---
+++ src/com/github/obsidianarch/gvengine/core/noise/PerlinNoise.java
@@ -0,0 +1,10 @@
+package com.github.obsidianarch.gvengine.core.noise;
+
+/**
+ * @author Austin
+ */
+public class PerlinNoise {
+    
+    // TODO
+    
+}
